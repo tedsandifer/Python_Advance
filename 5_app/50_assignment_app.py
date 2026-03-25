89b6ef3d2c9634f8e535c03ecf79de24f405774d
Ted Sandifer <tedsandifer@gmail.com>
2026-03-24 20:32:07 -0700

    Done

---
+++ 5_app/50_assignment_app.py
@@ -0,0 +1,86 @@
+"""
+content = assignment
+course  = Python Advanced
+ 
+date    = 14.11.2025
+email   = contact@alexanderrichtertd.com
+"""
+
+"""
+#*******************************************************************************
+# 50. CREATE UI
+#*******************************************************************************
+Create a UI for your application via code or designer.
+
+    a) Commit the "old" application version as "INIT 5_app"
+
+        • Complete your application
+        • Follow the UI design rules, make it pretty and make a screenshot.
+        • Record a one-minute video (with or without narration) showing the functionality of the app in a real-life scenario.
+          Make sure it "sells" your app to the audience, aka "How does it help the production?"
+
+        • BONUS: Integrate the app into your DCC (shelf, toolbar, menu, ...)
+
+    b) Commit as "DONE 5_app" and push the updated version to "main/master".
+
+
+
+
+
+#*******************************************************************************
+# 51. FINAL WORDS
+#*******************************************************************************
+
+    a) SHARE in the Discord/#finals group:
+
+    FINAL WORDS
+
+    * Which script editor will you use in the future and why?
+    
+After using Sublime and VS Code I have to say that in the future I will more than likely use Sublime. 
+I really like the simplified nature of Sublime. I find VS Code to be too cluttered and a tad overwhelming. 
+Maybe this is also due to sublime being the program I started coding on first, and therefore I feel more comfortable on it.
+
+    * Which topic/part made the biggest impact to your application?
+    
+The topic with the most impact would have to be the GUI chapter. 
+The idea of creating a user interface always seemed like a magic trick, 
+and knowing now how to perform is exciting. Creating GUI was always something super smart people did in my eyes. 
+I love writing functions, but putting them into a workflow that can be used by the average person is amazing.
+
+    * What are your future plans regarding your application?
+My application was something that I have always wanted at work. 
+As a layout artist, the camera and its attributes are constantly used. 
+So, having access to them quickly is a huge time saver. 
+I plan to keep developing tools and eventually a pipeline to use in the creation of my own projects. 
+My app was a big step in that direction as the knowledge I gained while building it has shown me the many skills 
+and the knowledge I need to get there.
+
+    * Your repository link (if not private)
+    [YOUR ANSWER]
+
+    Share your tool with our Discord group at #finals:
+	1. Explain what your tool does and why
+	2. Share images and video links
+    
+
+    b) Watch the Final Words chapter after submitting the final assignment.
+
+
+    c) Fill out "Your Reflection" form:
+       https://forms.gle/qRuv27PmaWhEx7PB8
+
+
+
+
+#*******************************************************************************
+# 52. SHARING
+#*******************************************************************************
+Share your tool on LinkedIn as well and tag us: 
+
+   www.linkedin.com/in/alexanderrichtertd
+   www.linkedin.com/company/alexanderrichtertd
+
+   This is an opportunity to show your progress in Python
+   with your current and future employers.
+"""
